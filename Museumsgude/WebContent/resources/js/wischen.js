8b9540b6e73990cb9d779cce21c59213e383f22f
ChrNikEsc <Chr.Nik.Esc@gmail.com>
2014-05-01 13:27:17 +0200

    1.5.14

--- Museumsgude/WebContent/resources/js/wischen.js
+++ Museumsgude/WebContent/resources/js/wischen.js
@@ -1,8 +1,35 @@
-$('body').live("swipeleft", function(){
-        var nextpage = $("#a").next('div[data-role="page"]');
+/*==Auf Seite A==*/
+
+$('body #a').live("swipeleft", function(){
+        var nextpage = $("#b");
         $.mobile.changePage(nextpage, 'slide');
       });
-      $('body').live("swiperight", function(){
-        var prevpage = $("#b").prev('div[data-role="page"]');
+
+$('body #a').live("swiperight", function(){
+       var prevpage = $("#c");
         $.mobile.changePage(prevpage, 'slide', true);
+      });
+
+/*==Auf Seite B==*/
+
+$('body #b').live("swipeleft", function(){
+        /*var nextpage = $("#a").next('div[data-role="page"]');
+        $.mobile.changePage(nextpage, 'slide');*/
+      });
+
+$('body #b').live("swiperight", function(){
+       var prevpage = $("#a");
+        $.mobile.changePage(prevpage, 'slide', true);
+      });
+
+/*==Auf Seite C==*/
+
+$('body #c').live("swipeleft", function(){
+        var nextpage = $("#a");
+        $.mobile.changePage(nextpage, 'slide');
+      });
+
+$('body #c').live("swiperight", function(){
+       /* var prevpage = $("#b").prev('div[data-role="page"]');
+        $.mobile.changePage(prevpage, 'slide', true);*/
       });
