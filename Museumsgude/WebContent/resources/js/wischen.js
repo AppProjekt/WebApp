a10b6e34d5bccdad725afe21474436602f052011
Sanok <Sanok@Sanok-PC>
2014-05-04 23:46:20 +0200

    Startseite mit Kacheln vorläufig fertig.

--- Museumsgude/WebContent/resources/js/wischen.js
+++ Museumsgude/WebContent/resources/js/wischen.js
@@ -33,3 +33,169 @@ $('body #c').live("swiperight", function(){
        /* var prevpage = $("#b").prev('div[data-role="page"]');
         $.mobile.changePage(prevpage, 'slide', true);*/
       });
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild1").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild2").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild3").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild4").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild5").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild6").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild7").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild8").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild9").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild10").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild11").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild12").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild13").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild14").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild15").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild16").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild17").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild18").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild19").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild20").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild21").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild22").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild23").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
+
+
+$('body #a').live('pagecreate', function(e){
+	$("#Bild24").click(function(e) {
+		$.mobile.changePage('#b', 'slide');
+	});
+});
