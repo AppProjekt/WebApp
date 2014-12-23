eb4bc07c953ce4e86906f30446e85585ad4c1038
ChrNikEsc <Chr.Nik.Esc@gmail.com>
2014-12-23 12:00:50 +0100

    Alle ggb-Dateien als html eingebaut

--- Museumsgude/WebContent/resources/js/wischen.js
+++ Museumsgude/WebContent/resources/js/wischen.js
@@ -44,10 +44,12 @@ $(".bilda").live("swipeleft", function(){
 $(".bilda").live("swiperight", function(){
 	var Hash = window.location.hash;
 	var wischBildnr = Hash.replace(/\D+/g,"");
+	var ggbQuelle = "resources/animationVideo/" + wischBildnr + ".html";
 	var C = "#" + wischBildnr + "c";
        var prevpage = $(C);
         $.mobile.changePage(prevpage, 'slide', true);
-        $("#ggb01").load("resources/animationVideo/animation_alt.html");
+        $(".bildcontent_ggb").load(ggbQuelle);
+        /*$("#ggb01").load("resources/animationVideo/animation_alt.html");*/
         /*$("#ggb02").load("resources/animationVideo/Gerstner Karl Fraktal.htm");
         $("#ggb03").load("resources/animationVideo/Max Bill Farbfeld.htm");
         $("#ggb04").load("resources/animationVideo/Gerstner Karl Fraktal.htm");
